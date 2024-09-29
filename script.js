a126c5414faf1cdeca489c20c176d78040c760e9
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-09-30 02:44:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your code here
+let parent = document.querySelector("#parent");
+var item = null
+var temp = null
+var item1 = null
+parent.addEventListener("dragstart", (e) => {
+    item = e.target.id;  
+	temp = e.target
+    console.log(item);
+});
+
+parent.addEventListener("dragover", (e) => {
+    e.preventDefault(); 
+});
+
+parent.addEventListener("drop", (e) => {
+    item1 = e.target.id; 
+     e.target.id = item
+	temp.id = item1
+});
